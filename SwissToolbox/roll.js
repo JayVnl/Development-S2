aae0b4e30cc7bbcf7d367453a78c765b45099b19
Jorrit Verstappen <47347978+JayVnl@users.noreply.github.com>
2019-10-30 13:18:18 +0100

    See description
    Changes:
    - Added my ez dice project to the toolbox
    - Changed the CSS to be more appealing to the eye

---
+++ SwissToolbox/roll.js
@@ -0,0 +1,24 @@
+var dice = document.getElementById("rollButton");
+dice.addEventListener("click", rollingAnimation, false);
+
+function roll() {
+  var randomNumber = Math.floor(Math.random() * Math.floor(6)) + 1;
+  if (randomNumber == 1) {
+    document.getElementById("dice").src = "images/d1.png";
+  } else if (randomNumber == 2) {
+    document.getElementById("dice").src = "images/d2.png";
+  } else if (randomNumber == 3) {
+    document.getElementById("dice").src = "images/d3.png";
+  } else if (randomNumber == 4) {
+    document.getElementById("dice").src = "images/d4.png";
+  } else if (randomNumber == 5) {
+    document.getElementById("dice").src = "images/d5.png";
+  } else if (randomNumber == 6) {
+    document.getElementById("dice").src = "images/d6.png";
+  }
+}
+
+function rollingAnimation() {
+  document.getElementById("dice").src = "images/diceRoll.gif";
+  setTimeout(roll, 2000);
+}
