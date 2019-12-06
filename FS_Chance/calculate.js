270c08ffd0b4f27b944e64d7fd7878d3f203f3b5
Jorrit Verstappen <47347978+JayVnl@users.noreply.github.com>
2019-12-06 19:27:48 +0100

    See description
    Changes:
    - Completed the algorithm so the application is fully functional with a few hiccups, see bugs and upcoming changes below.
    
    Bugs:
    1. Percentage sometimes adds 0.000000001.
    2. Doesn't show the new percentage when changing ONLY the item type.
    
    Fixes:
    1. Round the number at the end of calculate() to avoid the 0.000000001 being added.
    2. Clearing the item level value to avoid the percentage from not changing on item type change.
    
    Upcoming Updates:
    - Not showing item level +1 to +15 once the item type is accessory.
    - Not showing Item grades white and orange once the item type is accessory.
    - Adding responsiveness for mobile and other ratios different from a default monitor screen.
    - Changing current "about" placeholder text to something more useful. 
    - Making the Social buttons actually do something.

--- FS_Chance/calculate.js
+++ FS_Chance/calculate.js
@@ -63,18 +63,187 @@ function failstacks(element) {
 function calc() {
     if (itemT != undefined && itemG != undefined && itemL != undefined && itemF != undefined) {
         percentage = 0;
-    if (itemT == "armor" && itemL < 6) {
-        percentage = 100;
-    } else if (itemT == "weapon" && itemL < 8) {
-        percentage = 100;
-    } else if (ItemT == "weapon") {
+        if (itemT == "armor" && itemL < 5) {
+            percentage = 100;
+        } else if (itemT == "weapon" && itemL < 7) {
+            percentage = 100;
+        } else if (itemT == "armor" && (itemG == "white" || itemG == "blue" || itemG == "yellow") && itemL > 4) {
+            if (itemL == 5) {
+                percentage = 70;
+            } else if (itemL == 6) {
+                percentage = 25.64;
+            } else if (itemL == 7) {
+                percentage = 17.24;
+            } else if (itemL == 8) {
+                percentage = 11.76;
+            } else if (itemL == 9) {
+                percentage = 7.69;
+            } else if (itemL == 10) {
+                percentage = 6.25;
+            } else if (itemL == 11) {
+                percentage = 5;
+            } else if (itemL == 12) {
+                percentage = 4;
+            } else if (itemL == 13) {
+                percentage = 2.86;
+            } else if (itemL == 14) {
+                percentage = 2;
+            } else if (itemL == 15) {
+                percentage = 11.76;
+            } else if (itemL == 16) {
+                percentage = 7.69;
+            } else if (itemL == 17) {
+                percentage = 6.25;
+            } else if (itemL == 18) {
+                percentage = 2;
+            } else if (itemL == 19) {
+                percentage = 0.3;
+            }
+        } else if (itemT == "armor" && itemG == "green" && itemL > 4) {
+            if (itemL == 5) {
+                percentage = 70;
+            } else if (itemL == 6) {
+                percentage = 51.28;
+            } else if (itemL == 7) {
+                percentage = 17.24;
+            } else if (itemL == 8) {
+                percentage = 11.76;
+            } else if (itemL == 9) {
+                percentage = 7.69;
+            } else if (itemL == 10) {
+                percentage = 6.25;
+            } else if (itemL == 11) {
+                percentage = 5;
+            } else if (itemL == 12) {
+                percentage = 4;
+            } else if (itemL == 13) {
+                percentage = 3.75;
+            } else if (itemL == 14) {
+                percentage = 2;
+            } else if (itemL == 15) {
+                percentage = 11.76;
+            } else if (itemL == 16) {
+                percentage = 7.69;
+            } else if (itemL == 17) {
+                percentage = 6.25;
+            } else if (itemL == 18) {
+                percentage = 2;
+            } else if (itemL == 19) {
+                percentage = 0.3;
+            }
+        } else if (itemT == "weapon" && (itemG == "white" || itemG == "blue" || itemG == "yellow") && itemL > 6) {
+            if (itemL == 7) {
+                percentage = 70;
+            } else if (itemL == 8) {
+                percentage = 20.41;
+            } else if (itemL == 9) {
+                percentage = 14.29;
+            } else if (itemL == 10) {
+                percentage = 10;
+            } else if (itemL == 11) {
+                percentage = 6.67;
+            } else if (itemL == 12) {
+                percentage = 4;
+            } else if (itemL == 13) {
+                percentage = 2.5;
+            } else if (itemL == 14) {
+                percentage = 2;
+            } else if (itemL == 15) {
+                percentage = 11.76;
+            } else if (itemL == 16) {
+                percentage = 7.69;
+            } else if (itemL == 17) {
+                percentage = 6.25;
+            } else if (itemL == 18) {
+                percentage = 2;
+            } else if (itemL == 19) {
+                percentage = 0.3;
+            }
+        } else if (itemT == "weapon" && itemG == "green" && itemL > 6) {
+            if (itemL == 7) {
+                percentage = 70;
+            } else if (itemL == 8) {
+                percentage = 40.82;
+            } else if (itemL == 9) {
+                percentage = 14.29;
+            } else if (itemL == 10) {
+                percentage = 10;
+            } else if (itemL == 11) {
+                percentage = 6.67;
+            } else if (itemL == 12) {
+                percentage = 4;
+            } else if (itemL == 13) {
+                percentage = 3.75;
+            } else if (itemL == 14) {
+                percentage = 2;
+            } else if (itemL == 15) {
+                percentage = 11.76;
+            } else if (itemL == 16) {
+                percentage = 7.69;
+            } else if (itemL == 17) {
+                percentage = 6.25;
+            } else if (itemL == 18) {
+                percentage = 2;
+            } else if (itemL == 19) {
+                percentage = 0.3;
+            }
+        } else if (itemT == "weapon" && itemG == "orange" && itemL > 6) {
+            if (itemL == 7) {
+                percentage = 90;
+            } else if (itemL == 8) {
+                percentage = 20.41;
+            } else if (itemL == 9) {
+                percentage = 14.29;
+            } else if (itemL == 10) {
+                percentage = 10;
+            } else if (itemL == 11) {
+                percentage = 6.67;
+            } else if (itemL == 12) {
+                percentage = 4;
+            } else if (itemL == 13) {
+                percentage = 2.5;
+            } else if (itemL == 14) {
+                percentage = 2;
+            } else if (itemL == 15) {
+                percentage = 13.08;
+            } else if (itemL == 16) {
+                percentage = 10.63;
+            } else if (itemL == 17) {
+                percentage = 3.4;
+            } else if (itemL == 18) {
+                percentage = 0.51;
+            } else if (itemL == 19) {
+                percentage = 0.2;
+            }
+        } else if (itemT == "accessory" && itemG == "green" && itemL > 14) {
+            if (itemL == 15) {
+                percentage = 30;
+            } else if (itemL == 16) {
+                percentage = 10;
+            } else if (itemL == 17) {
+                percentage = 7.5;
+            } else if (itemL == 18) {
+                percentage = 2.5;
+            } else if (itemL == 19) {
+                percentage = 0.5;
+            }
+        } else if (itemT == "accessory" && (itemG == "blue" || itemG == "yellow") && itemL > 14) {
+            if (itemL == 15) {
+                percentage = 25;
+            } else if (itemL == 16) {
+                percentage = 10;
+            } else if (itemL == 17) {
+                percentage = 7.5;
+            } else if (itemL == 18) {
+                percentage = 2.5;
+            } else if (itemL == 19) {
+                percentage = 0.5;
+            }
+        }
 
-    }
-
-    calcChance();
-    showPercentage();
-    }
-    else {
+        calcChance();
+        showPercentage();
+    } else {
         alert("Please fill in all the fields");
     }
 }
