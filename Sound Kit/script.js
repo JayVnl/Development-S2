4e9f1c34453c98cf1161684ac64cd1ea60db673c
Jorrit Verstappen <47347978+JayVnl@users.noreply.github.com>
2019-12-15 15:33:52 +0100

    See description
    Changes:
    - Added sound for each block
    - Added functionality to block to play the audio file
    - Added 2 key events for the first 2 blocks

--- Sound Kit/script.js
+++ Sound Kit/script.js
@@ -14,82 +14,182 @@ function generateBoard() {
   }
 };
 
+// Play sound according to what block is clicked on
 function play(element) {
   switch (element.id) {
     case "block0":
-      console.log("You just pressed block 0");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/drum-kicks.wav');
+      audio.play();
       break;
     case "block1":
-      console.log("You just pressed block 1");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/snare-drum.wav');
+      audio.play();
       break;
     case "block2":
-      console.log("You just pressed block 2");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/drum-sticks.wav');
+      audio.play();
       break;
     case "block3":
-      console.log("You just pressed block 3");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/drum-3.wav');
+      audio.play();
       break;
     case "block4":
-      console.log("You just pressed block 4");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/bass-drum.wav');
+      audio.play();
       break;
     case "block5":
-      console.log("You just pressed block 5");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/peep.wav');
+      audio.play();
       break;
     case "block6":
-      console.log("You just pressed block 6");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/claves-4.wav');
+      audio.play();
       break;
     case "block7":
-      console.log("You just pressed block 7");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/blip.wav');
+      audio.play();
       break;
     case "block8":
-      console.log("You just pressed block 8");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/tick.wav');
+      audio.play();
       break;
     case "block9":
-      console.log("You just pressed block 9");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/rim.wav');
+      audio.play();
       break;
     case "block10":
-      console.log("You just pressed block 10");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/beep-2.wav');
+      audio.play();
       break;
     case "block11":
-      console.log("You just pressed block 11");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/bass-1.wav');
+      audio.play();
       break;
     case "block12":
-      console.log("You just pressed block 12");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/drums-1.wav');
+      audio.play();
       break;
     case "block13":
-      console.log("You just pressed block 13");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/metal-1.wav');
+      audio.play();
       break;
     case "block14":
-      console.log("You just pressed block 14");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/beep-3.wav');
+      audio.play();
       break;
     case "block15":
-      console.log("You just pressed block 15");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/random-1.wav');
+      audio.play();
       break;
     case "block16":
-      console.log("You just pressed block 16");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/snare-1.wav');
+      audio.play();
       break;
     case "block17":
-      console.log("You just pressed block 17");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/kick-low.wav');
+      audio.play();
       break;
     case "block18":
-      console.log("You just pressed block 18");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/snare-w.wav');
+      audio.play();
       break;
     case "block19":
-      console.log("You just pressed block 19");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/kick-chip.wav');
+      audio.play();
       break;
     case "block20":
-      console.log("You just pressed block 20");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/blip-4.wav');
+      audio.play();
       break;
     case "block21":
-      console.log("You just pressed block 21");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/wow.mp3');
+      audio.play();
       break;
     case "block22":
-      console.log("You just pressed block 22");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/scratch.wav');
+      audio.play();
       break;
     case "block23":
-      console.log("You just pressed block 23");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/warning.wav');
+      audio.play();
       break;
     case "block24":
-      console.log("You just pressed block 24");
+      element.classList.add('animation');
+      setTimeout(removeAnimation, 500);
+      var audio = new Audio('sounds/slash.wav');
+      audio.play();
       break;
+  }
+}
+
+document.onkeyup = function(e) {
+  var e = e || window.event;
+  if (e.which == 81) {
+    var id = document.getElementById('block0');
+    var audio = new Audio('sounds/drum-kicks.wav');
+    audio.play();
+    id.classList.add('animation');
+    setTimeout(removeAnimation, 500);
+  } else if (e.which == 87) {
+    var id = document.getElementById('block1');
+    var audio = new Audio('sounds/snare-drum.wav');
+    audio.play();
+    id.classList.add('animation');
+    setTimeout(removeAnimation, 500);
+  }
+}
+
+function removeAnimation() {
+  var blocks = ["block0", "block1", "block2", "block3", "block4", "block5", "block6", "block7", "block8", "block9", "block10", "block11", "block12", "block13", "block14", "block15", "block16", "block17", "block18", "block19", "block20", "block21", "block22", "block23", "block24"]
+  for (i = 0; i < blocks.length; i++) {
+    document.getElementById(blocks[i]).classList.remove('animation');
   }
 }
