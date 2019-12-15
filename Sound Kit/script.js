c610ab694f837957bcc8a51ec40abf8031470bc6
Jorrit Verstappen <47347978+JayVnl@users.noreply.github.com>
2019-12-15 12:57:39 +0100

    See description
    Changes:
    - Added an unique ID to each block generated 
    - Comments added in the code to show what each part does so far

--- Sound Kit/script.js
+++ Sound Kit/script.js
@@ -1,9 +1,15 @@
 function generateBoard() {
+  // Generating the divs that make the blocks for the soundboard
   var container = document.getElementById("container");
-  var amount = 1;
   for (var i = 0; i < 25; i++) {
     var box = "box";
     container.innerHTML += '<div class="box"><div class="inner-box"</div></div>';
-    amount++;
+  }
+
+  // Assigning a unique ID to each block
+  var id = "block";
+  var items = document.getElementsByClassName("inner-box");
+  for (i = 0; i < items.length; i++) {
+    items[i].id = id + i;
   }
 };
